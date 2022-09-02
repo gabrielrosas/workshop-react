cae5009e6c180623c8ec71ca0d14df5280ed96a3
Gabriel Rosas <me@gabrielrosas.com>
2022-09-02 16:11:16 -0300

    criando branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ import './App.css';
 import CampoBusca from './Componentes/CampoBusca';
 import ListaFilmes from './Componentes/ListaFilmes';
 
+const recordsMock = [{
+  titulo: "The Avengers",
+  imgUrl: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+  ano: 2012,
+  id: "tt0848228",
+},{
+  titulo: "Avengers: Endgame",
+  imgUrl: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+  ano: 2019,
+  id: "tt4154796",
+},{
+  titulo: "Avengers: Infinity War",
+  imgUrl: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg",
+  ano: 2018,
+  id: "tt4154756",
+}]
+
 function App() {
   return (
     <Container text className="containerPage">
@@ -18,7 +35,7 @@ function App() {
       <CampoBusca/>
       <Divider/>
 
-      <ListaFilmes/>
+      <ListaFilmes records={recordsMock}/>
     </Container>
   );
 }
--- src/Componentes/ListaFilmes.js
+++ src/Componentes/ListaFilmes.js
@@ -5,25 +5,19 @@ import {
 
 import ItemFilme from './ItemFilme'
 
-function ListaFilmes() {
+function ListaFilmes({ records }) {
     return (
         <Segment>
             <Item.Group divided>
-                <ItemFilme
-                    titulo="The Avengers"
-                    imgUrl="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-                    ano="2012"
-                />
-                <ItemFilme
-                    titulo="Avengers: Endgame"
-                    imgUrl="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
-                    ano="2019"
-                />
-                <ItemFilme
-                    titulo="Avengers: Infinity War"
-                    imgUrl="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg"
-                    ano="2018"
-                />
+                {records.map((record) => (
+                    <ItemFilme
+                        key={record.id}
+                        imgUrl={record.imgUrl}
+                        titulo={record.titulo}
+                        ano={record.ano}
+                        id={record.id}
+                    />
+                ))}
             </Item.Group>
         </Segment>
     )
